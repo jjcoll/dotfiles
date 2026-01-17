5d23345967541c1c617c2573950df3657b1ad7f9
jjcoll <jjordicoll@gmail.com>
2026-01-17 03:17:15 +0100

    added nvim image preview support with snacks

---
+++ nvim/lua/plugins/image.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      image = {
+        enabled = true,
+        backend = "kitty",
+        doc = {
+          enabled = true,
+          inline = true,
+          float = true,
+        },
+      },
+    },
+  },
+}
